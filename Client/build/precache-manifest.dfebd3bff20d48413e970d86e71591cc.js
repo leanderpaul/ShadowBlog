119513c7ba9247930fa87ffaedbe5356f132cd50
leanderpaul <leanderpaul97@gmail.com>
2019-04-07 07:27:35 +0530

    removed build from gitignore

---
+++ Client/build/precache-manifest.dfebd3bff20d48413e970d86e71591cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4bf5424aa1000872f07e",
+    "url": "/static/js/main.4a36773c.chunk.js"
+  },
+  {
+    "revision": "ebf79f89fece4ac08057",
+    "url": "/static/js/2.73bbeeb7.chunk.js"
+  },
+  {
+    "revision": "4bf5424aa1000872f07e",
+    "url": "/static/css/main.669c07de.chunk.css"
+  },
+  {
+    "revision": "ebf79f89fece4ac08057",
+    "url": "/static/css/2.a8bca99a.chunk.css"
+  },
+  {
+    "revision": "48618a95169f3adf1691fb9fabf98029",
+    "url": "/index.html"
+  }
+];
